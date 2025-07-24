ee3c5b1c549a2a1b54ddba2f6f67986ce20ea679
yyeojung <hongshii0i0i@gmail.com>
2025-07-24 21:37:59 +0900

    cors package 추가

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/api", indexRouter);
 
 const mongoURI = `mongodb://localhost:27017/todo-demo`;
