e32b208993cac185c96eb4d4226b1d67083932cd
yyeojung <hongshii0i0i@gmail.com>
2025-07-25 00:06:49 +0900

    배포 URI 수정

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+require("dotenv").config();
 const cors = require("cors");
-
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api", indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
   .connect(mongoURI /*, { useNewUrlParser: true }*/)
@@ -20,7 +22,7 @@ mongoose
     console.log(error);
   });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server on 5000");
 });
 
