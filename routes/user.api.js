05ea88a3a89e98807736bbb558751c0d189651a3
yyeojung <hongshii0i0i@gmail.com>
2025-07-26 19:53:51 +0900

    로그인 api

--- routes/user.api.js
+++ routes/user.api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user.controller");
 
-// 1. 회원가입 endpoint
-router.post("/", userController.createUser);
+router.post("/", userController.createUser); // 회원가입 endpoint
+router.post("/login", userController.loginWithEmail); // 로그인 endpoint
 
 module.exports = router;
