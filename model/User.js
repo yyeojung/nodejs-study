12aa4a6b572ae55e34f4fa3e19436da4f38864dc
yyeojung <hongshii0i0i@gmail.com>
2025-07-26 19:52:35 +0900

    비밀번호 삭제, 토큰생성

--- model/User.js
+++ model/User.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const userSchema = Schema(
   {
@@ -18,6 +21,18 @@ const userSchema = Schema(
   },
   { timestamps: true }
 );
+// 언제든지 지워야 하는거 ** 비밀번호는 아예 삭제해서 전달
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  return obj;
+};
 
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
+};
 const User = mongoose.model("User", userSchema);
 module.exports = User;
